e93fe4b2cea83ca6780d9dbac66e1d1240ed2586
Mats Nyrenius <mnyrenius@gmail.com>
2020-04-23 20:47:50 +0200

    add some kind of distortion effect

--- effects/src/fx2.cc
+++ effects/src/fx2.cc
@@ -1,23 +1,49 @@
 #include <audio_processor.h>
 #include <fx_plugin.h>
-#include <cstring>
 #include <memory>
+#include <cmath>
 
 namespace awesomefx
 {
 
+namespace
+{
+  inline auto distort(auto in, auto x)
+  {
+    if (in < 0.0)
+    {
+      return std::pow(in + 1.0, x) - 1.0;
+    }
+    else
+    {
+      return std::pow(in - 1.0, x) + 1.0;
+    }
+  }
+}
+
 class SimpleDistortion : public AudioProcessor
 {
   public:
+    SimpleDistortion()
+    {
+      m_params = {1};
+    }
+
     void process(Sample* in_l, Sample* in_r, Sample* out_l, Sample* out_r, std::size_t numSamples) override
     {
-      std::memcpy(out_l, in_l, sizeof(Sample) * numSamples);
-      std::memcpy(out_r, in_r, sizeof(Sample) * numSamples);
+      while (numSamples--)
+      {
+        *out_l++ = distort(*in_l++, m_params[0]);
+        *out_r++ = distort(*in_r++, m_params[0]);
+      }
     }
 
     void setParameter(const AudioProcessor::Parameter& param) override
     {
+      m_params[param.index] = param.value;
     }
+
+    std::vector<int32_t> m_params;
 };
 
 class SimpleDistortionPlugin : public FxPlugin
@@ -25,7 +51,7 @@ class SimpleDistortionPlugin : public FxPlugin
   public:
     FxPluginInfo getPluginInfo() const override
     {
-      return {"SimpleDistortion", {"Param1", "Param2"}};
+      return {"SimpleDistortion", {"Krschh"}};
     }
 
     AudioProcessor::Ptr createAudioProcessor() const override
