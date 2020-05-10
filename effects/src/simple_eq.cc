9b47c1916c71a0178a3771604ed05f0fc55fd1d6
Mats Nyrenius <mnyrenius@gmail.com>
2020-05-10 13:07:41 +0200

    simple_eq: add cut

--- effects/src/simple_eq.cc
+++ effects/src/simple_eq.cc
@@ -2,6 +2,7 @@
 #include <fx_plugin.h>
 #include <memory>
 #include <cmath>
+#include <type_traits>
 
 namespace awesomefx
 {
@@ -17,63 +18,128 @@ const float CutoffMid = 1000;
 const float CutoffHigh = 8000;
 const float BandwidthMid = 800;
 
-template<int Sign>
+struct LowPass{};
+struct HighPass{};
+
+template<typename TFilter>
 class ShelvingFilter
 {
   public:
-    inline void calcCoeffs(std::uint32_t fs, float cutoff)
+    ShelvingFilter(std::uint32_t fs, float cutoff)
+      : m_fs(fs),
+        m_cutoff(cutoff)
     {
-      m_c_b = (std::tan(3.141592 * cutoff / fs) - 1) / (std::tan(3.141592 * cutoff / fs) + 1);
+      calcCoeffs();
+    }
+
+    inline void calcCoeffs()
+    {
+      auto tan = std::tan(3.141592 * m_cutoff / m_fs);
+      m_c_b = (tan - 1) / (tan + 1);
+      if (std::is_same<TFilter, LowPass>::value)
+      {
+        m_c_c = (tan - m_v0) / (tan + m_v0);
+      }
+      else
+      {
+        m_c_c = m_v0 * (tan - 1) / m_v0 * (tan + 1);
+      }
     }
 
     inline void setGain(float gain)
     {
-      m_v0 = std::pow(10.0, (gain * 24) / 20.0);
+      gain -= 0.5;
+      m_v0 = std::pow(10.0, gain * 48 / 20.0);
+      if (gain >= 0.0)
+      {
+        m_c = m_c_b;
+      }
+      else
+      {
+        calcCoeffs();
+        m_c = m_c_c;
+      }
     }
 
     inline Sample filter(Sample in)
     {
-      auto xh_new = in - m_c_b * m_x_h;
-      auto y_l = m_c_b * xh_new + m_x_h;
+      auto xh_new = in - m_c * m_x_h;
+      auto y_l = m_c * xh_new + m_x_h;
       m_x_h = xh_new;
-      return 0.5 * (m_v0 - 1) * (in + y_l * Sign) + in;
+      if (std::is_same<TFilter, LowPass>::value)
+      {
+        return 0.5 * (m_v0 - 1) * (in + y_l) + in;
+      }
+      else
+      {
+        return 0.5 * (m_v0 - 1) * (in - y_l) + in;
+      }
     }
 
   private:
-    float m_c_b = 0;
-    float m_v0 = 0;
-    float m_x_h = 0;
+    float m_c_b{};
+    float m_c_c{};
+    float m_v0{};
+    float m_x_h{};
+    float m_c{};
+    float m_fs{};
+    float m_cutoff{};
 };
 
 class PeakFilter
 {
   public:
-    inline void calcCoeffs(std::uint32_t fs, float cutoff, float bandwidth)
+    PeakFilter(std::uint32_t fs, float cutoff, float bandwidth)
+      : m_fs(fs)
+      , m_cutoff(cutoff)
+      , m_bandwidth(bandwidth)
     {
-      m_d = -std::cos(2.0 * 3.141592 * cutoff / fs);
-      m_c_b = (std::tan(3.141592 * bandwidth / fs) - 1) / (std::tan(3.141592 * bandwidth / fs) + 1);
+      calcCoeffs();
+    }
+
+    inline void calcCoeffs()
+    {
+      auto tan = std::tan(3.141592 * m_bandwidth / m_fs) ;
+      m_d = -std::cos(2.0 * 3.141592 * m_cutoff / m_fs);
+      m_c_b = (tan - 1) / (tan + 1);
+      m_c_c = (tan - m_v0) / (tan + m_v0);
     }
 
     inline void setGain(float gain)
     {
-      m_v0 = std::pow(10, gain * 32.0 / 20.0);
+      gain -= 0.5;
+      m_v0 = std::pow(10, gain * 48.0 / 20.0);
+      if (gain >= 0.0)
+      {
+        m_c = m_c_b;
+      }
+      else
+      {
+        calcCoeffs();
+        m_c = m_c_c;
+      }
     }
 
     inline Sample filter(Sample in)
     {
-      auto xh = in - m_d * (1 - m_c_b) * m_x_h_1 + m_c_b * m_x_h_2;
-      auto y_l = -m_c_b * xh + m_d * (1 - m_c_b) * m_x_h_1 + m_x_h_2;
+      auto xh = in - m_d * (1 - m_c) * m_x_h_1 + m_c * m_x_h_2;
+      auto y_l = -m_c * xh + m_d * (1 - m_c) * m_x_h_1 + m_x_h_2;
       m_x_h_1 = xh;
       m_x_h_2 = m_x_h_1;
       return 0.5 * (m_v0 - 1) * (in - y_l) + in;
     }
 
   private:
-    float m_x_h_1 = 0;
-    float m_x_h_2 = 0;
-    float m_v0 = 0;
-    float m_d = 0;
-    float m_c_b = 0;
+    float m_x_h_1{};
+    float m_x_h_2{};
+    float m_v0{};
+    float m_d{};
+    float m_c_b{};
+    float m_c_c{};
+    float m_c{};
+    float m_fs{};
+    float m_cutoff{};
+    float m_bandwidth{};
 };
 }
 
@@ -82,13 +148,13 @@ class SimpleEq : public AudioProcessor
   public:
     SimpleEq(const AudioProcessingContext& context)
       : m_fs(context.getSampleRate())
+      , m_low_l(m_fs, CutoffLow)
+      , m_low_r(m_fs, CutoffLow)
+      , m_high_l(m_fs, CutoffHigh)
+      , m_high_r(m_fs, CutoffHigh)
+      , m_peak_l(m_fs, CutoffMid, BandwidthMid)
+      , m_peak_r(m_fs, CutoffMid, BandwidthMid)
     {
-      m_low_l.calcCoeffs(m_fs, CutoffLow);
-      m_low_r.calcCoeffs(m_fs, CutoffLow);
-      m_high_l.calcCoeffs(m_fs, CutoffHigh);
-      m_high_r.calcCoeffs(m_fs, CutoffHigh);
-      m_peak_l.calcCoeffs(m_fs, CutoffMid, BandwidthMid);
-      m_peak_r.calcCoeffs(m_fs, CutoffMid, BandwidthMid);
     }
 
     void process(Sample* in_l, Sample* in_r, Sample* out_l, Sample* out_r, std::size_t numSamples) override
@@ -132,10 +198,10 @@ class SimpleEq : public AudioProcessor
 
   private:
     float m_fs;
-    ShelvingFilter<1> m_low_l;
-    ShelvingFilter<-1> m_high_l;
-    ShelvingFilter<1> m_low_r;
-    ShelvingFilter<-1> m_high_r;
+    ShelvingFilter<LowPass> m_low_l;
+    ShelvingFilter<HighPass> m_high_l;
+    ShelvingFilter<LowPass> m_low_r;
+    ShelvingFilter<HighPass> m_high_r;
     PeakFilter m_peak_l;
     PeakFilter m_peak_r;
 
